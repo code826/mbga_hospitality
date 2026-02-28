b54a71f507228f541f315013798ce488787c7fca
Vikas Singh <vikassingh@Vikass-MacBook-Air.local>
2026-02-28 14:15:29 +0530

    added-all-commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Essential for GitHub Pages relative paths
+  base: '/mbga_hospitality/', // Correct base path for your GitHub repo
 })
